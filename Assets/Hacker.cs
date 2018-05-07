9d92118eeaee36bb230804194edbb6e8bda741cc
Adam <Twilightsparkleadam@gmail.com>
2018-05-07 16:52:18 +0300

    Added welcome text
    Created Hacker script for the game

---
+++ Assets/Hacker.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hacker : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Terminal.WriteLine("Welcome to the Hacker game.");
+        Terminal.WriteLine("Choose your difficult.");
+        // For the empty line
+        Terminal.WriteLine(" ");
+
+        Terminal.WriteLine("1. Easy. You'll hack a library");
+        Terminal.WriteLine(" ");
+
+        Terminal.WriteLine("2. Medium. You'll hack a police station");
+        Terminal.WriteLine("3. Hard. You'll hack the Pentagon base");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
